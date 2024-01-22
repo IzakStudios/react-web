6a02f7eb233ff4616f8a7e4477daf10206fb2edd
IzakDev <141975061+IzakStudios@users.noreply.github.com>
2024-01-22 09:35:41 +0000

    a

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../stylesheets/Home.css"
+
+function Home (){
+    return <div className="Home">
+        <div className="Topbar"></div>
+        <div className="Content"></div>
+        <div className="Footer"></div>
+    </div>
+}
+
+export default Home;
